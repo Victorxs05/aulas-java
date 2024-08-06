7e3aaf9aa0bf367ce38a7e7e3cbe04ff89677f7d
Victorxs05 <160060912+Victorxs05@users.noreply.github.com>
2024-08-06 11:23:41 -0300

    Add files via upload

---
+++ Funcionarios.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author aluno.den
+ */
+public class Funcionarios {
+    private String nome;
+    private String funcao;
+    private double salario;
+    
+    // Construtor 
+
+    public Funcionarios(String nome, String funcao, double salario) {
+        this.nome = nome;
+        this.funcao = funcao;
+        this.salario = salario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    
+    
+}
