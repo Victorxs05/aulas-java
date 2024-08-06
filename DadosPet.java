6b1aa411184feb7fa80ffe225c95ace03125d2b9
Victorxs05 <160060912+Victorxs05@users.noreply.github.com>
2024-08-06 11:24:11 -0300

    Add files via upload

---
+++ DadosPet.java
@@ -0,0 +1,70 @@
+package com.mycompany.pet;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author aluno.den
+ */
+public class DadosPet {
+    private String nome;
+    private int idade;
+    private String raca;
+    private String porte;
+    private String alimentacao;
+    
+    // Contrutor.
+
+    public DadosPet(String nome, int idade, String raca, String porte, String alimentacao) {
+        this.nome = nome;
+        this.idade = idade;
+        this.raca = raca;
+        this.porte = porte;
+        this.alimentacao = alimentacao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public String getPorte() {
+        return porte;
+    }
+
+    public void setPorte(String porte) {
+        this.porte = porte;
+    }
+
+    public String getAlimentacao() {
+        return alimentacao;
+    }
+
+    public void setAlimentacao(String alimentacao) {
+        this.alimentacao = alimentacao;
+    }
+    
+    
+}
