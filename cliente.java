d8a75239f864d2f64b2732aae195f2c8937abe7d
Victorxs05 <160060912+Victorxs05@users.noreply.github.com>
2024-08-06 11:23:32 -0300

    Add files via upload

---
+++ cliente.java
@@ -0,0 +1,40 @@
+package com.mycompany.mavenproject1;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author aluno.den
+ */
+public class cliente {
+    private String nome;
+    private int idade;
+
+    // Construtor.
+    public cliente(String nome, int idade) {    
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    // Métodos de acesso.
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+    
+}
