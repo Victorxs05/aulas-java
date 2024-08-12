f1185548b1350196f98c451e35aabc48492f37fe
Victorxs05 <160060912+Victorxs05@users.noreply.github.com>
2024-08-12 10:14:25 -0300

    Add files via upload

---
+++ Dados.java
@@ -0,0 +1,134 @@
+package com.mycompany.aulahoje;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author aluno.den
+ */
+public class Dados {
+    private String nome;
+    private int idade;
+    private int cpf;
+    private String endereco;
+    private int telefone;
+
+    public Dados(String nome, int idade, int cpf, String endereco, int telefone) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+    
+    // Veiculo
+    
+    private String placa;
+    private String cor;
+    private int numeroPassageiros;
+    private double capacidadeTanque;
+    private double velocidadeMaxima;
+    private double consumoMedio;
+
+    public Dados(String placa, String cor, int numeroPassageiros, double capacidadeTanque, double velocidadeMaxima, double consumoMedio) {
+        this.placa = placa;
+        this.cor = cor;
+        this.numeroPassageiros = numeroPassageiros;
+        this.capacidadeTanque = capacidadeTanque;
+        this.velocidadeMaxima = velocidadeMaxima;
+        this.consumoMedio = consumoMedio;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getNumeroPassageiros() {
+        return numeroPassageiros;
+    }
+
+    public void setNumeroPassageiros(int numeroPassageiros) {
+        this.numeroPassageiros = numeroPassageiros;
+    }
+
+    public double getCapacidadeTanque() {
+        return capacidadeTanque;
+    }
+
+    public void setCapacidadeTanque(double capacidadeTanque) {
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    public double getVelocidadeMaxima() {
+        return velocidadeMaxima;
+    }
+
+    public void setVelocidadeMaxima(double velocidadeMaxima) {
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+    public double getConsumoMedio() {
+        return consumoMedio;
+    }
+
+    public void setConsumoMedio(double consumoMedio) {
+        this.consumoMedio = consumoMedio;
+    }
+    
+    
+}
